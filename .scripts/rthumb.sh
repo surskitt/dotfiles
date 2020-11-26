7b32c0c0e781301cf7b5b9d7955bd5108b55d58b
shanedabes <shane@isda.best>
2020-11-26 01:05:47 +0000

    use comicthumb to thumbnail comic archives and storage thumbnails relative to dir

--- .scripts/rthumb.sh
+++ .scripts/rthumb.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-OUTPUT_DIR=~/.thumbnails/zips
+# OUTPUT_DIR=~/.thumbnails/zips
+
 VERBOSE=false
 while getopts 'vo:h' opt; do
   case "$opt" in
@@ -22,18 +23,24 @@ while getopts 'vo:h' opt; do
 done
 shift $(( OPTIND - 1 ))
 
+# mkdir -p "${OUTPUT_DIR}"
+# mkdir -p /tmp/rthumb
+
+OUTPUT_DIR="$(dirname "${1}")/.thumbs"
 mkdir -p "${OUTPUT_DIR}"
-mkdir -p /tmp/rthumb
 
-rsum=$(echo -n "${1}"|sha1sum|cut -d ' ' -f 1)
+FN="$(basename "${1}")"
 
-[ -f "${OUTPUT_DIR}/${rsum}" ] || {
-    thumb_file="$(unrar lb "${1}"|egrep 'jpg|png'|sort|head -1)"
+rsum=$(echo -n "${FN}"|sha1sum|cut -d ' ' -f 1)
+
+[ -f "${OUTPUT_DIR}/${rsum}.jpg" ] || {
+    # thumb_file="$(unrar lb "${1}"|egrep 'jpg|png'|sort|head -1)"
     # unzip -o "${1}" -d /tmp "${thumb_file/\[/\\[}"
     # mv "/tmp/${thumb_file}" "${OUTPUT_DIR}/${zsum}"
     # unrar x "${1}" "${thumb_file//\[/\\[}" > "${OUTPUT_DIR}/${zsum}"
-    ( cd /tmp/rthumb && unrar x -inul -y "${1}" "${thumb_file}" )
-    mv "/tmp/rthumb/${thumb_file}" "${OUTPUT_DIR}/${rsum}"
+    # ( cd /tmp/rthumb && unrar x -inul -y "${1}" "${thumb_file}" )
+    # mv "/tmp/rthumb/${thumb_file}" "${OUTPUT_DIR}/${rsum}"
+    comicthumb "${1}" "${OUTPUT_DIR}/${rsum}.jpg" 512
 }
 
-$VERBOSE && echo "${OUTPUT_DIR}/${rsum}"
+$VERBOSE && echo "${OUTPUT_DIR}/${rsum}.jpg"
