55155da3c0e78e33b937aa11442e5dddfad259c8
shanedabes <shane@isda.best>
2021-01-30 00:17:11 +0000

    add host arg and sort by date

--- .scripts/fzf-mpc.sh
+++ .scripts/fzf-mpc.sh
@@ -1,5 +1,36 @@
 #!/usr/bin/env bash
 
+usage() {
+    echo "nah"
+}
+
 export TAB="	"
 
-mpc listall -f "[%albumartist%|%artist%] - %album%${TAB}%file%"|while read a; do echo "${a%/*}"; done|sort -u|fzf --layout=reverse-list -m -d "${TAB}" --with-nth 1|cut -d "${TAB}" -f 2|while read s; do mpc add "${s}"; done
+MPD_HOST=localhost
+
+while getopts 'H:h' opt; do
+    case "${opt}" in
+        H)
+            MPD_HOST="${OPTARG}"
+            ;;
+        h)
+            usage
+            exit
+            ;;
+        ?)
+            usage >&2
+            exit 1
+    esac
+done
+shift $(( OPTIND - 1 ))
+
+mpc -h "${MPD_HOST}" listall -f "[%albumartist%|%artist%] - %album%${TAB}%mdate%${TAB}%file%" | \
+    while read -r a; do echo "${a%/*}"; done | \
+    sort -u | \
+    sort -t "${TAB}" -k 2.7n -k 2.8n -k 2.1n -k 2.2n -k 2.4n -k 2.5n | \
+    tac | \
+    fzf +s --layout=reverse-list -m -d "${TAB}" --with-nth 1 | \
+    cut -d "${TAB}" -f 3 | \
+    while read -r s; do
+        mpc -h "${MPD_HOST}" add "${s}"
+    done
