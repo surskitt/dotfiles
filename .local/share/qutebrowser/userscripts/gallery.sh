849b54a47fcbebca43ae6295971eb379a6436470
shanedabes <shane@isda.best>
2021-03-21 23:32:36 +0000

    pipe gallery.sh to feh

--- .local/share/qutebrowser/userscripts/gallery.sh
+++ .local/share/qutebrowser/userscripts/gallery.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-gallery_dl() {
-    tempdir=$(mktemp -d)
-    echo "jseval \"Downloading gallery... \"" >> "$QUTE_FIFO" 
-    gallery-dl -d "${tempdir}" "${1}"
-    for i in "${tempdir}"/*/*; do
-        mv "${i}" "${i// /_}"
-    done
-    # echo "jseval \"Combining images... \"" >> "$QUTE_FIFO"
-    # find "${tempdir}" -type f|sort|tail -n +2|xargs -n2|while read a b; do
-    # find "${tempdir}" -type f|sort|xargs -n2|while read a b; do
-    #    convert "$b" "$a" +append "$a"
-    #    rm "$b"
-    # done
-    feh -Z -Y -r -F "${tempdir}"
-}
+# gallery_dl() {
+#     tempdir=$(mktemp -d)
+#     echo "jseval \"Downloading gallery... \"" >> "$QUTE_FIFO" 
+#     gallery-dl -d "${tempdir}" "${1}"
+#     for i in "${tempdir}"/*/*; do
+#         mv "${i}" "${i// /_}"
+#     done
+#     # echo "jseval \"Combining images... \"" >> "$QUTE_FIFO"
+#     # find "${tempdir}" -type f|sort|tail -n +2|xargs -n2|while read a b; do
+#     # find "${tempdir}" -type f|sort|xargs -n2|while read a b; do
+#     #    convert "$b" "$a" +append "$a"
+#     #    rm "$b"
+#     # done
+#     feh -Z -Y -r -F "${tempdir}"
+# }
 
 # gallery_dl "$1"
 
 echo "jseval \"Opening $1 using gallery-dl... \"" >> "$QUTE_FIFO"
-gallery_dl "$1" || echo "jseval \"Opening $1 using gallery-dl... failed\"" >> "$QUTE_FIFO"
+gallery-dl -g "${1}" | feh -f -
+# gallery_dl "$1" || echo "jseval \"Opening $1 using gallery-dl... failed\"" >> "$QUTE_FIFO"
