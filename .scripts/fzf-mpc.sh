af5299f4299c9c7d8bed763e078d82be6e6c6abc
shanedabes <shane@isda.best>
2021-03-24 18:38:52 +0000

    remove sort +s flag

--- .scripts/fzf-mpc.sh
+++ .scripts/fzf-mpc.sh
@@ -33,7 +33,7 @@ mpc -h "${MPD_HOST}" listall -f "[%albumartist%|%artist%] - %album%${TAB}%mdate%
     sort -u | \
     sort -t "${TAB}" -k 2.7n -k 2.8n -k 2.1n -k 2.2n -k 2.4n -k 2.5n | \
     tac | \
-    fzf +s --layout=reverse-list -m -d "${TAB}" --with-nth 1 | \
+    fzf --layout=reverse-list -m -d "${TAB}" --with-nth 1 | \
     cut -d "${TAB}" -f 3 | \
     while read -r s; do
         mpc -h "${MPD_HOST}" "${COMMAND}" "${s}"
