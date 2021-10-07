4bce222e8f8f9bfb2d86a4b9371b659648f688af
shanedabes <shane@isda.best>
2021-10-08 00:45:20 +0100

    add script to pick random albums for daapd queue

---
+++ .scripts/fzf-daapd-random.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+usage() {
+    echo "something"
+}
+
+TAB="	"
+
+albums_json="$(curl -s http://mallard.lan:3689/api/library/albums)"
+
+# echo "${albums_json}"
+
+album_list="$(jq -r '.items[]|"\(.artist) - \(.name)	\(.uri)	\(.time_added)"' <<< "${albums_json}" | sort -t '	' -k 3 -r | shuf -n 50 )"
+
+selected="$(fzf -m -d '	' --with-nth=1 --layout=reverse-list <<< "${album_list}"|cut -d '	' -f 2)"
+
+if [[ -z "${selected}" ]]; then
+    exit 1
+fi
+
+args="$(tr '\n' ',' <<< "${selected}" ; echo)"
+
+curl -s -X POST "http://mallard.lan:3689/api/queue/items/add?uris=${args}"
