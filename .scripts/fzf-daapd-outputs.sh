e1e2b07f4cd848c4cc8f5d700c5113005d75e021
shanedabes <shane@isda.best>
2021-12-20 00:40:46 +0000

    fzf daapd output script handles cancelling selection

--- .scripts/fzf-daapd-outputs.sh
+++ .scripts/fzf-daapd-outputs.sh
@@ -4,6 +4,10 @@ outputs=$(curl -s http://mallard.lan:3689/api/outputs|jq -r '.outputs[]|"\(.id)	
 
 selected="$(fzf -m -d '	' --with-nth=2 --layout=reverse-list <<< "${outputs}"|cut -d '	' -f 1)"
 
+if [[ -z "${selected}" ]]; then
+    exit 1
+fi
+
 list="$(for i in ${selected}; do echo \"${i}\"; done | paste -s -d, -)"
 
 curl -X PUT "http://mallard.lan:3689/api/outputs/set" --data "{\"outputs\":[${list}]}"
