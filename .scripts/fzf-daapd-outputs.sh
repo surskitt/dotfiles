da946337e536b945aa3035f06799b7a5a34d2ae7
shanedabes <shane@isda.best>
2021-12-20 00:38:57 +0000

    add fzf script to set daapd outputs

---
+++ .scripts/fzf-daapd-outputs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+outputs=$(curl -s http://mallard.lan:3689/api/outputs|jq -r '.outputs[]|"\(.id)	\(.name)"')
+
+selected="$(fzf -m -d '	' --with-nth=2 --layout=reverse-list <<< "${outputs}"|cut -d '	' -f 1)"
+
+list="$(for i in ${selected}; do echo \"${i}\"; done | paste -s -d, -)"
+
+curl -X PUT "http://mallard.lan:3689/api/outputs/set" --data "{\"outputs\":[${list}]}"
