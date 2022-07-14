534c1c97afd534f4c18f0b3b14d38f6f4430fa83
shanedabes <shane@isda.best>
2022-07-14 11:00:50 +0100

    fix dmenu scripts

--- .scripts/dmenu.sh
+++ .scripts/dmenu.sh
@@ -3,4 +3,4 @@
 # Import the colors
 . "${HOME}/.cache/wal/colors.sh"
 
-dmenu_run -F -nb "$background" -nf "$foreground" -sb "$color0" -sf "$color7"
+dmenu_run -nb "$background" -nf "$foreground" -sb "$color0" -sf "$color7"
