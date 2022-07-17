14f9a0174d69793d0be63129c508dc8d7bbd894a
shanedabes <shane@isda.best>
2022-07-17 10:26:52 +0100

    add select all binding to fzf random scripts

--- .scripts/fzf-daapd-random.sh
+++ .scripts/fzf-daapd-random.sh
@@ -12,7 +12,7 @@ albums_json="$(curl -s http://mallard.lan:3689/api/library/albums)"
 
 album_list="$(jq -r '.items[]|"\(.artist) - \(.name)	\(.uri)	\(.time_added)"' <<< "${albums_json}" | sort -t '	' -k 3 -r | shuf -n 50 )"
 
-selected="$(fzf -m -d '	' --with-nth=1 --layout=reverse-list <<< "${album_list}"|cut -d '	' -f 2)"
+selected="$(fzf -m -d '	' --with-nth=1 --layout=reverse-list --bind ctrl-a:select-all <<< "${album_list}"|cut -d '	' -f 2)"
 
 if [[ -z "${selected}" ]]; then
     exit 1
