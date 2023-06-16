dd6a18be5d8b572a4ef5e2e548a20026fee76518
shanedabes <shane@isda.best>
2023-06-16 21:46:14 +0100

    default transparency 90%

--- .scripts/st-toggle-transparency.sh
+++ .scripts/st-toggle-transparency.sh
@@ -5,7 +5,7 @@ current_alpha="$(xrdb -query -get alpha)"
 if [[ "${current_alpha}" != 1 ]]; then
     new_alpha=1
 else
-    new_alpha=0.85
+    new_alpha=0.90
 fi
 
 cat << EOF > ~/.Xresources.d/xst_alpha.Xresources
