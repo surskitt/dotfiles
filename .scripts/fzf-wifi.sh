f058dcb24209d3f4a5d70ece689571a65857dd65
shanedabes <shane@isda.best>
2023-07-12 21:08:12 +0100

    add wifi script

---
+++ .scripts/fzf-wifi.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+nmcli -f 'bssid,signal,bars,freq,ssid' --color yes device wifi list --rescan yes | \
+    fzf --with-nth=2.. --ansi --height=40% --reverse --cycle --inline-info --header-lines=1  | \
+    tr -s ' ' | \
+    cut -d ' ' -f 6 | \
+    xargs -I {} sudo nmcli device wifi connect {} password "${WIFI_PASS}"
