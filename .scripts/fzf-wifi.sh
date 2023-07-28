ee039eaf6e8749976908d178c85dfa1ebfe9fce5
shanedabes <shane@isda.best>
2023-07-28 16:08:11 +0100

    get wifi password from gopass in fzf-wifi script

--- .scripts/fzf-wifi.sh
+++ .scripts/fzf-wifi.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+WIFI_PASS="$(gopass wifi)"
+
 nmcli -f 'bssid,signal,bars,freq,ssid' --color yes device wifi list --rescan yes | \
     fzf --with-nth=2.. --ansi --height=40% --reverse --cycle --inline-info --header-lines=1  | \
     tr -s ' ' | \
