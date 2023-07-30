6020a7aea7ec19d9301ed1bef2f5bda50c34b0ce
shanedabes <shane@isda.best>
2023-07-30 08:34:15 +0100

    add fterm plugin to neovim

--- .config/nvim/lua/plugins/nord.lua
+++ .config/nvim/lua/plugins/nord.lua
@@ -6,7 +6,17 @@ return {
         vim.g.nord_disable_background = true
         vim.g.nord_italic = false
         vim.g.nord_bold = false
+        vim.g.nord_borders = true
+
+        grpid = vim.api.nvim_create_augroup('custom_highlights_nord', {})
+        vim.api.nvim_create_autocmd('ColorScheme', {
+            group = grpid,
+            pattern = 'nord',
+            command = 'highlight FloatBorder guibg=NONE ctermbg=NONE',
+        })
 
         vim.cmd([[colorscheme nord]])
+        -- require('nord').set()
+        -- vim.cmd([[highlight VertSplit guibg=NONE ctermbg=NONE]])
     end
 }
