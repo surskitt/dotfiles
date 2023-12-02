548188569a849460f6ad4458f6516495426857a8
surskitt <surskitt@users.noreply.github.com>
2023-12-02 22:16:00 +0000

    reset daapd outputs if the same are picked

--- .scripts/fzf-daapd-outputs.sh
+++ .scripts/fzf-daapd-outputs.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-outputs=$(curl -s http://mallard.lan:3689/api/outputs|jq -r '.outputs[]|"\(.id)	\(.name)"')
+outputs=$(curl -s http://mallard.lan:3689/api/outputs)
+list=$(jq -r '.outputs[]|"\(.id)	\(.name)"' <<< "${outputs}")
 
-selected="$(fzf -m -d '	' --with-nth=2 --layout=reverse-list <<< "${outputs}"|cut -d '	' -f 1)"
+selected="$(fzf -m -d '	' --with-nth=2 --layout=reverse-list <<< "${list}" | cut -d '	' -f 1 | sort)"
 
 if [[ -z "${selected}" ]]; then
     exit 1
 fi
 
-list="$(for i in ${selected}; do echo \"${i}\"; done | paste -s -d, -)"
+current="$(jq '.outputs[] | select(.selected == true)|.id' <<< "${outputs}" | sort | paste -sd ,)"
+new="$(for i in ${selected}; do echo \"${i}\"; done | paste -s -d, -)"
 
-curl -X PUT "http://mallard.lan:3689/api/outputs/set" --data "{\"outputs\":[${list}]}"
+if [[ "${current}" == "${new}" ]] ; then
+    curl -X PUT "http://mallard.lan:3689/api/outputs/set" --data "{\"outputs\":[]}"
+fi
+
+curl -X PUT "http://mallard.lan:3689/api/outputs/set" --data "{\"outputs\":[${new}]}"
