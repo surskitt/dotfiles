ef76cff1b5de8ef1862112d14b1a76a97376c3c7
surskitt <surskitt@users.noreply.github.com>
2024-01-23 15:30:53 +0000

    feat(scripts): use manual images for sx_select

--- .scripts/sx_select.sh
+++ .scripts/sx_select.sh
@@ -2,51 +2,52 @@
 
 TAB="	"
 
-if [ "${#}" -eq 0 ]; then
-    dir="."
-else
-    dir="${1}"
-fi
+shopt -s extglob
+shopt -s nullglob
 
-if ! [ -d "${dir}" ]; then
-    echo "error: ${dir} is not a directory" >&2
-    exit 1
-fi
+# if [ "${#}" -eq 0 ]; then
+#     dir="${PWD}"
+# else
+#     dir="${1}"
+# fi
 
-mkdir -p "${dir}/.thumbs"
-THUMB_FILE=${dir}/.thumbs/thumbs.tsv
+fs=(
+    */.folder
+    *.jpg
+    *.gif
+    *.png
+    .thumbs/!(*-pt[2-9]).jpg
+    .thumbs/*.gif
+)
 
-no_refresh=true
-
-current="$(ls -l1 "${dir}" "${dir}"/*/.folder 2>/dev/null)"
-if [ -f "${dir}/.thumbs/last.txt" ]; then
-    last="$(< "${dir}/.thumbs/last.txt")"
-fi
-
-if [[ "${last}" != "${current}" ]]; then
-    no_refresh=false
-fi
-
-echo "${current}" > "${dir}/.thumbs/last.txt"
-
-if [[ -f "${THUMB_FILE}" && $no_refresh == true ]]; then
-    fl=$(sort -t "${TAB}" -k 2 "${THUMB_FILE}")
-else
-    f=$(find "${dir}" -maxdepth 1 -not -path '*/\.*' -not -path "${dir}" -not -iname '*-pt[2-9]*')
-    
-    fl=$(echo "${f}"|while read -r fn; do
-        p=$(previewer.sh "${fn}")
-
-        echo "${p}${TAB}${fn##*/}"
-    done|sort -t "${TAB}" -k 2)
-
-    echo "${fl}" > "${THUMB_FILE}"
-fi
-
-s=$(echo "${fl}"|while IFS=$"${TAB}" read -r fn _; do echo "${fn}"; done|sxiv -ftio|head -1)
-
-if [[ "${s}" == "" ]]; then
+if [[ "${#fs[@]}" -eq 0 ]] ; then
+    echo "No thumbs found"
     exit
 fi
 
-grep "${s}" <<< "${fl}"|cut -d "${TAB}" -f 2
+# selected="$(
+#     for img in "${fs[@]}" ; do
+#         echo "${img}"
+#     done | sxiv -ftio | head -1
+# )"
+
+selected="$(sxiv -afto "${fs[@]}" | head -1)"
+
+if [[ -z "${selected}" ]] ; then
+    echo "No thumb selected"
+    exit
+fi
+
+case "${selected}" in
+    */.folder)
+        echo "${selected%/.folder}"
+        ;;
+    .thumbs/*)
+        fn="${selected#*/}"
+        fnn="${fn%.*}"
+        ls "${fnn}".* 2>/dev/null | head -1
+        ;;
+    *)
+        echo "${selected}"
+        ;;
+esac
