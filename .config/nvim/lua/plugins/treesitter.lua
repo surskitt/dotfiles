827b08c976b89bedb85d07143ae0e7bce04ea3e3
surskitt <surskitt@users.noreply.github.com>
2024-02-09 23:58:40 +0000

    feat(vim): enable treesitter autoinstall

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -28,7 +28,7 @@ return {
                 "yaml",
             },
             sync_install = false,
-            auto_install = false,
+            auto_install = true,
             highlight = {
                 enable = true,
                 disable = { "css" },
