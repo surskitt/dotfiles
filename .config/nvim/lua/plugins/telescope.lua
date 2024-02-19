228a1961a0c1a291ce1b22bfea87ae5c02685582
surskitt <surskitt@users.noreply.github.com>
2024-02-19 22:00:51 +0000

    feat(vim): telescope open multiple files with enter

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,18 @@
+local select_one_or_multi = function(prompt_bufnr)
+    local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+    local multi = picker:get_multi_selection()
+    if not vim.tbl_isempty(multi) then
+        require('telescope.actions').close(prompt_bufnr)
+        for _, j in pairs(multi) do
+            if j.path ~= nil then
+                vim.cmd(string.format('%s %s', 'edit', j.path))
+            end
+        end
+    else
+        require('telescope.actions').select_default(prompt_bufnr)
+    end
+end
+
 return {
     {
         'nvim-telescope/telescope.nvim',
@@ -28,7 +43,11 @@ return {
                             ["<C-k>"] = actions.move_selection_previous,
                             ["<C-j>"] = actions.move_selection_next,
                             ["<C-u>"] = false,
-                        }
+                            ["<CR>"]  = select_one_or_multi,
+                        },
+                        n = {
+                            ["<CR>"] = select_one_or_multi,
+                        },
                     }
                 },
                 extensions = {
