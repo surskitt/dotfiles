18f16948af561e59e1a45ce2ffd800073a8c3869
surskitt <surskitt@users.noreply.github.com>
2024-05-21 15:22:45 +0100

    feat(scripts): save balatro backups to home dir

--- .scripts/balatro-save-backup.sh
+++ .scripts/balatro-save-backup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 BALATRO_DIR="${HOME}/.local/share/Steam/steamapps/compatdata/2379780/pfx/drive_c/users/steamuser/AppData/Roaming/Balatro"
-BACKUP_DIR="${BALATRO_DIR}/backups/$(date "+%Y%m%d_%H%M%S")"
+BACKUP_DIR="${HOME}/backups/balatro/saves/$(date "+%Y%m%d_%H%M%S")"
 
 mkdir -p "${BACKUP_DIR}"
 
