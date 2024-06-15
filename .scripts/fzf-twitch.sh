50d7d64ab9a47662f4ac02e12d054fd56d233e62
surskitt <surskitt@users.noreply.github.com>
2024-06-15 15:27:21 +0100

    feat(scripts): update fzf twitch script to use ttvchecker

--- .scripts/fzf-twitch.sh
+++ .scripts/fzf-twitch.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ "${1}" == "cached" ]]; then
-    if [[ -f /tmp/twitch_subes ]]; then
-        chans="$(tail -1 /tmp/twitch_subes | xargs -n 1)"
-    else
-        chans="$(twitchnotifier -c sharktamer -n|cut -d ':' -f 1)"
-    fi
-else
-    chans="$(twitchnotifier -c sharktamer -n|cut -d ':' -f 1)"
-fi
+selected="$(cut -d ',' -f 1 ~/.cache/ttvchecker.csv | fzf +m --layout=reverse-list)"
 
-t=$(fzf --layout=reverse-list <<< "${chans}")
-
-if [ -z "${t}" ]; then
+if [[ -z "${selected}" ]] ; then
     exit 1
 fi
 
-disown.sh streamlink --twitch-low-latency --player mpv "https://twitch.tv/${t}" best
-disown.sh floater.sh st -g 60x40+20-50 tiny_twitch.sh "${t}"
+xdg-open "https://twitch.tv/${selected}"
