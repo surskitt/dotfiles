93b59b8297f7b58f33538e795e725b8f49219518
surskitt <surskitt@users.noreply.github.com>
2026-03-28 23:14:41 +0000

    feat(scripts): add category flag to qb search script

--- .scripts/qb_search.py
+++ .scripts/qb_search.py
@@ -4,10 +4,12 @@ import argparse
 import subprocess
 
 import qbittorrentapi
+import humanize
 
 parser = argparse.ArgumentParser(prog="qb_add", description="add torrent files to qbittorrent")
 
 parser.add_argument("-q", "--quiet", action="store_true")
+parser.add_argument("-c", "--category")
 parser.add_argument("query")
 
 args = parser.parse_args()
@@ -17,11 +19,20 @@ qb_host = f"https://qb.{mallard_domain}"
 
 client = qbittorrentapi.Client(host=qb_host)
 
-torrents = client.torrents_info()
+torrents = client.torrents_info(category=args.category)
 
-for t in torrents:
-    if args.query.lower() in t.name.lower():
-        if args.quiet:
-            print(t.hash)
-        else:
-            print(t.name)
+searched_torrents = [t for t in torrents if args.query.lower() in t.name.lower()]
+
+for n, t in enumerate(searched_torrents, 1):
+    if args.quiet:
+        print(t.hash)
+        continue
+
+    print(t.name)
+    print(humanize.naturalsize(t.size, binary=True))
+    print(t.category)
+    print(t.save_path)
+    print(t.hash)
+
+    if n < len(searched_torrents):
+        print()
