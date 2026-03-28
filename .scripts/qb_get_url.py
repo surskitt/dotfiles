a3eabe0e0a093fee071e312f2b26380101c333bc
surskitt <surskitt@users.noreply.github.com>
2026-03-28 23:07:24 +0000

    feat(scripts): add qb get url script

---
+++ .scripts/qb_get_url.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+
+import qbittorrentapi
+
+mallard_domain = subprocess.run(["gopass", "mallard_domain"], capture_output=True, encoding="utf-8").stdout.strip()
+qb_host = f"https://qb.{mallard_domain}"
+
+client = qbittorrentapi.Client(host=qb_host)
+
+torrents = client.torrents_info(torrent_hashes=sys.argv[1:])
+
+for i in torrents:
+    print(i["comment"])
