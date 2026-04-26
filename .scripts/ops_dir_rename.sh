ef79b1cf51136a2fc3559afc5104272b40313076
surskitt <surskitt@users.noreply.github.com>
2026-04-26 23:06:28 +0100

    feat(scripts): use metaflac in ops dir rename script

--- .scripts/ops_dir_rename.sh
+++ .scripts/ops_dir_rename.sh
@@ -15,31 +15,22 @@ if [[ -z "${firstflac}" ]] ; then
 fi
 
 get_tag() {
-    metaflac --show-tag="${1}" "${2}" | cut -d = -f 2
+    metaflac --show-tag="${1}" "${2}" | cut -d = -f 2 | tr '\n' '_' | sed 's/_/ _ /g;s#/#_#g;s/ _ $//'
 }
 
-json="$(mediainfo --Output=JSON "${firstflac}")"
+artist="$(get_tag ARTIST "${firstflac}")"
+album="$(get_tag ALBUM "${firstflac}")"
+year="$(get_tag YEAR "${firstflac}")"
+label="$(get_tag LABEL "${firstflac}")"
+bitdepth="$(metaflac --show-bps "${firstflac}")"
+samplingrate="$(metaflac --show-sample-rate "${firstflac}")"
 
-artist="$(jq -r '.media.track[0].Album_Performer' <<< "${json}" | sed 's#/#_#g')"
-album="$(jq -r '.media.track[0].Album' <<< "${json}" | sed 's#/#_#g')"
-recorded_date="$(jq -r '.media.track[0].Recorded_Date' <<< "${json}" | cut -d '/' -f 2 | xargs)"
-year="${recorded_date%%-*}"
-label="$(jq -r '.media.track[0].Label' <<< "${json}" | sed 's#/#_#g')"
-bitdepth="$(jq -r '.media.track[1].BitDepth' <<< "${json}")"
-samplingrate="$(jq -r '.media.track[1].SamplingRate' <<< "${json}")"
-
-album_more="$(jq -r '.media.track[0].Title_More' <<< "${json}" | sed 's#/#_#g')"
-if [[ "${album_more}" != null ]] ; then
-    album="${album} (${album_more})"
+version="$(get_tag VERSION "${firstflac}")"
+if [[ -n "${version}" ]] ; then
+    album="${album} (${version})"
 fi
 
-# upc="$(jq -r '.media.track[0].extra.upc' <<< "${json}" | sed 's#/#_#g')"
-# upc="$(metaflac --show-tag=UPC "${firstflac}" | cut -d = -f 2)"
 upc="$(get_tag UPC "${firstflac}")"
-
-if [[ "${upc}" == "null" ]] ;  then
-    upc="$(jq -r '.media.track[0].extra.CATALOGNUMBER' <<< "${json}" | sed 's#/#_#g')"
-fi
 
 if [[ "${samplingrate}" == "44100" ]] ; then
     samplingrate="44.1"
@@ -53,5 +44,4 @@ if [[ "${rddir}" == "${rd}" ]] ; then
     rddir=.
 fi
 
-# echo "${rddir}/${artist} - ${album} (${year}) {${label}, ${upc}} [WEB FLAC ${bitdepth}-${samplingrate}]"
 mv "${rd}" "${rddir}/-${artist} - ${album} (${year}) {${label}, ${upc}} [WEB FLAC ${bitdepth}-${samplingrate}]"
