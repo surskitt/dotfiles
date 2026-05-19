237cee2f00a305ae9f4fe7b6766ff83869a667c1
surskitt <surskitt@users.noreply.github.com>
2026-05-20 00:21:48 +0100

    feat(scripts): downsize art before applying

--- .scripts/flac_add_album_art.sh
+++ .scripts/flac_add_album_art.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
-if [[ "${#}" -lt 1 ]] ; then
+if [[ "${#}" -lt 1 ]]; then
     echo "Error: missing cover art file name" >&2
     exit 1
 fi
 
 art_file="${1}"
 
-for i in *.flac ; do
+magick "${art_file}" -resize '1000x1000>' aa.jpg
+jpegoptim -m 85 aa.jpg
+
+ls -lh *.jpg
+
+for i in *.flac; do
     metaflac --remove --block-type=PICTURE,PADDING --dont-use-padding "${i}"
-    metaflac --import-picture-from="${art_file}" "${i}"
+    metaflac --import-picture-from=aa.jpg "${i}"
 done
+
+rm aa.jpg
