bc63b575b55873e7c58bc617cba4097a5419ed76
surskitt <surskitt@users.noreply.github.com>
2026-05-20 00:21:23 +0100

    fix(scripts): copy 16 bit flacs in downconv script

--- .scripts/dst.sh
+++ .scripts/dst.sh
@@ -4,19 +4,16 @@ downsampler-threaded.sh .
 
 mkdir ../dst
 
-# ds_dir="$(ls -d resampled-16-*)"
-
-# for i in * ; do
-    # if [[ "${i}" != *.flac ]] ; then
-    #     echo "Copying ${i}"
-    #     cp "${i}" "${ds_dir}"/
-    # fi
-# done
-
-find . -maxdepth 1 -type f \! -iname '*.flac' | while read -r f ; do
-  cp "${f}" ../dst/
+find . -maxdepth 1 -type f \! -iname '*.flac' | while read -r f; do
+    cp "${f}" ../dst/
 done
 
-# mv "${ds_dir}" ..
 mv */*.flac ../dst
+
+for i in *.flac; do
+    if metaflac --show-bps -- "${i}" | grep -q '16$'; then
+        cp "${i}" ../dst
+    fi
+done
+
 ops_dir_rename.sh ../dst
