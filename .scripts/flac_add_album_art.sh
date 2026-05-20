76ed03538a6d8f071918a515330426b819cd0175
surskitt <surskitt@users.noreply.github.com>
2026-05-20 01:09:12 +0100

    feat(scripts): check album art size before applying

--- .scripts/flac_add_album_art.sh
+++ .scripts/flac_add_album_art.sh
@@ -10,9 +10,22 @@ fi
 magick "${art_file}" -resize '1000x1000>' aa.jpg
 jpegoptim -m 85 aa.jpg
 
+album_art_size="$(wc -c < aa.jpg)"
+
 ls -lh *.jpg
 
+echo
+
 for i in *.flac; do
+    current_art_size="$(metaflac "${i}" --export-picture-to=- | wc -c)"
+
+    if [[ "${current_art_size}" -eq "${album_art_size}" ]] ; then
+        echo "${i} - Album art size unchanged"
+        continue
+    fi
+
+    echo "${i} - Applying album art"
+
     metaflac --remove --block-type=PICTURE,PADDING --dont-use-padding "${i}"
     metaflac --import-picture-from=aa.jpg "${i}"
 done
