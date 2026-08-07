cf851bcd4a99d51a7bebb35bb43c5b10e60cd5ca
surskitt <surskitt@users.noreply.github.com>
2026-08-07 12:14:12 +0100

    feat(scripts): handle renaming cd release dirs

--- .scripts/ops_dir_rename.py
+++ .scripts/ops_dir_rename.py
@@ -26,36 +26,56 @@ if len(flacs) == 0:
     print("Error: no flacs found", file=sys.stderr)
     sys.exit(1)
 
-album_artists = {" _ ".join(f.tags.get("ALBUMARTIST")) for _, f in flacs}
+album_artists = {
+    (
+        "Various Artists"
+        if len(f.tags.get("ALBUMARTIST")) >= 3
+        else " _ ".join(f.tags.get("ALBUMARTIST"))
+    )
+    for _, f in flacs
+}
+
 albums = {f.tags.get("ALBUM")[0] for _, f in flacs}
 years = {f.tags.get("YEAR")[0] for _, f in flacs}
+releaseyears = {
+    f.tags.get("RELEASEYEAR")[0] for _, f in flacs if f.tags.get("RELEASEYEAR")
+}
 labels = {f.tags.get("LABEL")[0] for _, f in flacs}
-upcs = {f.tags.get("UPC")[0] for _, f in flacs}
-versions = {f.tags.get("VERSION")[0] for _, f in flacs if f.tags.get("VERSION")}
+upcs = {f.tags.get("UPC")[0] for _, f in flacs if f.tags.get("UPC")}
+catnos = {
+    f.tags.get("CATALOGNUMBER")[0] for _, f in flacs if f.tags.get("CATALOGNUMBER")
+}
+versions = {
+    f.tags.get("ALBUMVERSION")[0] for _, f in flacs if f.tags.get("ALBUMVERSION")
+}
 
 for tags, msg in [
     (album_artists, "directory flacs contain more than one set of album artists"),
     (albums, "directory flacs contain more than one album"),
     (years, "directory flacs contain more than one year"),
+    (releaseyears, "directory flacs contain more than one releaseyear"),
     (labels, "directory flacs contain more than one label"),
     (upcs, "directory flacs contain more than one upc"),
+    (catnos, "directory flacs contain more than one upc"),
     (versions, "directory flacs contain more than one version"),
 ]:
     if len(tags) > 1:
         print(f"Error: {msg}", file=sys.stderr)
         sys.exit(1)
 
+upccatnos = upcs or catnos or {""}
+# ryears = releaseyears or years
+ryears = years
+
 album_artist = album_artists.pop()
 album = albums.pop()
-year = years.pop()
+ryear = ryears.pop()
 label = labels.pop()
-upc = upcs.pop()
+upccatno = upccatnos.pop()
 
-album = album.replace("/", "_")
-
-bit_depth = min(f.info.bits_per_sample for _, f in flacs)
+bit_depth = max(f.info.bits_per_sample for _, f in flacs)
 sample_rate = str(
-    round(min(f.info.sample_rate for _, f in flacs) / 1000, 1)
+    round(max(f.info.sample_rate for _, f in flacs) / 1000, 1)
 ).removesuffix(".0")
 
 if len(versions) == 1:
@@ -63,7 +83,21 @@ if len(versions) == 1:
 
     album = f"{album} ({version})"
 
-new_dir_name = f"{args.prefix}{album_artist} - {album} ({year}) {{{label}, {upc}}} [WEB FLAC {bit_depth}-{sample_rate}]"
+release_type = "WEB"
+flac_info = f"FLAC {bit_depth}-{sample_rate}"
+
+if len(list(args.dir.glob("**/*.log")) + list(args.dir.glob("**/*.cue"))) > 1:
+    release_type = "CD"
+    flac_info = "FLAC"
+
+new_dir_name = f"{args.prefix}{album_artist} - {album} ({ryear}) {{{label}, {upccatno}}} [{release_type} {flac_info}]"
+
+for i in ["/", "?", '"', "*", "|"]:
+    new_dir_name = new_dir_name.replace(i, "_")
+
+for i in [":"]:
+    new_dir_name = new_dir_name.replace(i, "-")
+
 new_dir = args.dir.parent.joinpath(new_dir_name)
 
 if args.dir != new_dir:
